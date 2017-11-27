c64127a318566e8f50ac529fc9470605b4898432
Lobozel <lobozel@outlook.es>
2017-11-27 12:09:07 +0100

    Update

--- PT/EjerciciosClase/Triangulo.java
+++ PT/EjerciciosClase/Triangulo.java
@@ -1,78 +1,82 @@
 package EjerciciosClase;
 
+import EjerciciosClase.Punto;
+
+/**
+ * @author MiguelAngel
+ */
 public class Triangulo {
-private int lado1;
-private int lado2;
-private int lado3;
+    private Punto Vertice1, Vertice2, Vertice3;
 
-public Triangulo() {
-}
+    public Triangulo() {
+        Punto punto1=new Punto(3,4);
+        Punto punto2=new Punto();
+        Punto punto3=new Punto(1,-2);
+        Vertice1 = punto1;
+        Vertice2 = punto2;
+        Vertice3 = punto3;
+    }
 
-public Triangulo(int lado1, int lado2, int lado3) {
-	super();
-	this.lado1 = lado1;
-	this.lado2 = lado2;
-	this.lado3 = lado3;
-}
+    public Triangulo(Triangulo t) {
+        Vertice1 = t.Vertice1;
+        Vertice2 = t.Vertice2;
+        Vertice3 = t.Vertice3;
+    }
 
-public Triangulo(Triangulo t) {
-	this.lado1 = t.lado1;
-	this.lado2 = t.lado2;
-	this.lado3 = t.lado3;
-}
+    public Triangulo(Punto vertice1, Punto vertice2, Punto vertice3) {
+        Vertice1 = vertice1;
+        Vertice2 = vertice2;
+        Vertice3 = vertice3;
+    }
 
-public int getLado1() {
-	return lado1;
-}
+    public Punto getVertice1() {
+        return Vertice1;
+    }
 
-public void setLado1(int lado1) {
-	this.lado1 = lado1;
-}
+    public Punto getVertice2() {
+        return Vertice2;
+    }
 
-public int getLado2() {
-	return lado2;
-}
+    public Punto getVertice3() {
+        return Vertice3;
+    }
 
-public void setLado2(int lado2) {
-	this.lado2 = lado2;
-}
+    public void setVertice1(Punto vertice1) {
+        Vertice1 = vertice1;
+    }
 
-public int getLado3() {
-	return lado3;
-}
+    public void setVertice2(Punto vertice2) {
+        Vertice2 = vertice2;
+    }
 
-public void setLado3(int lado3) {
-	this.lado3 = lado3;
-}
+    public void setVertice3(Punto vertice3) {
+        Vertice3 = vertice3;
+    }
 
-@Override
-public String toString() {
-	return "Triangulo [lado1=" + lado1 + ", lado2=" + lado2 + ", lado3=" + lado3 + "]";
-}
+    @Override
+    public String toString() {
+        return "A(" + Vertice1 +
+                ") B(" + Vertice2 +
+                ") C(" + Vertice3 +
+                ')';
+    }
 
-public int area() {
-	double p = (lado1+lado2+lado3)/2.0;
-	double area = Math.pow(p*(p-lado1)*(p-lado2)*(p-lado2),1/2);
-	return (int)area;
-}
+    public double perimetro(){
+        Punto punto1=new Punto(this.getVertice1());
+        Punto punto2=new Punto(this.getVertice2());
+        Punto punto3=new Punto(this.getVertice3());
+        return punto1.distancia(punto2)+punto2.distancia(punto3)+punto3.distancia(punto1);
+    }
 
-public void areaString() {
-	double p = (lado1+lado2+lado3)/2.0;
-	double area = Math.pow(p*(p-lado1)*(p-lado2)*(p-lado2),1/2);
-	System.out.println("El área es "+area);
-}
+    public double area(){
+        Punto punto1=new Punto(this.getVertice1());
+        Punto punto2=new Punto(this.getVertice2());
+        Punto punto3=new Punto(this.getVertice3());
+        double semiperimetro = this.perimetro()/2;
+        return Math.pow(semiperimetro*(Math.pow((semiperimetro-punto1.distancia(punto2)),2)*Math.pow((semiperimetro-punto2.distancia(punto3)), 2)*Math.pow((semiperimetro-punto2.distancia(punto3)),2)),0.5);
+    }
 
-public int Area(int lado1, int lado2, int lado3) {
-	double p = (lado1+lado2+lado3)/2.0;
-	double area = Math.pow(p*(p-lado1)*(p-lado2)*(p-lado2),1/2);
-	return (int)area;
-}
+    public static void main(String[] args) {
 
-public void AreaString(int lado1, int lado2, int lado3) {
-	double p = (lado1+lado2+lado3)/2.0;
-	double area = Math.pow(p*(p-lado1)*(p-lado2)*(p-lado2),1/2);
-	System.out.println("El área es "+area);
-
-}
-
+    }
 }
