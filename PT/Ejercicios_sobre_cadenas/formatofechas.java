f413e9b8d599dbef4682b7bc14b054e3997e9569
Lobozel <>
2018-01-29 16:33:54 +0100

    Update

--- PT/Ejercicios_sobre_cadenas/formatofechas.java
+++ PT/Ejercicios_sobre_cadenas/formatofechas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * 
- * @author MiguelÃ�ngel
+ * @author Miguelángel
  *
  */
 public class formatofechas {
@@ -12,7 +12,7 @@ public class formatofechas {
  * Hacer un programa que al recibir como dato una cadena de caracteres
  * que represente una fecha en formato (dd/mm/aa), genere otra cadena
  * con la misma fecha pero con un formato (dd de nombre del mes de aaaa).
- * Por ejemplo si se ingresa la cadena: 20/11/2006 la otra cadena serÃ¡: 20
+ * Por ejemplo si se ingresa la cadena: 20/11/2006 la otra cadena será: 20
  * de Noviembre de 2006
  */
 	public static String cambioDeFormato(String f) {
@@ -45,19 +45,44 @@ public class formatofechas {
 		}
 		return fecha[0]+" de "+fecha[1]+" de "+fecha[2];
 	}
+	
+	protected static boolean bisiesto(int año){
+		boolean bisiesto=false;
+		if(año%4==0 || año%400==0 && año%100!=0)
+			bisiesto=true;
+		return bisiesto;
+	}
+	
+	public static int diasMes(int mes, int año){
+		int dias=0; 
+		switch (mes){
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			dias=31;
+			break;
+		case 4: case 6: case 9: case 11:
+			dias=30;
+			break;
+		case 2:
+			if(bisiesto(año)) 
+				dias=29;
+			else
+				dias=28;
+		}
+		return dias;
+	}
+	
 	public static void main(String[] args) {
 	Scanner entrada = new Scanner(System.in);
 	String fecha;
-//	do {
-//	System.out.println("Introduce una fecha con el formato dd/mm/aaaa");
-//	fecha=entrada.nextLine();
-//	}while();
-//	}while(hora.length() != 5 || hora.charAt(2) != ':' ||
-//			Integer.parseInt(hora.substring(0, 2)) > 23 ||
-//			Integer.parseInt(hora.substring(0, 2)) < 0 ||
-//			Integer.parseInt(hora.substring(3, 5)) > 59 ||
-//Integer.parseInt(hora.substring(3, 5)) < 0);
-//	System.out.println(cambioDeFormato(fecha));
+	do {
+	System.out.println("Introduce una fecha con el formato dd/mm/aaaa");
+	fecha=entrada.nextLine();
+	}while(fecha.length()!=10 || fecha.charAt(2)!='/' || fecha.charAt(5)!='/' ||
+	Integer.parseInt(fecha.substring(3, 5))<1 || Integer.parseInt(fecha.substring(3, 5))>12 
+	|| Integer.parseInt(fecha.substring(0,2))<1
+	|| Integer.parseInt(fecha.substring(0,2))>diasMes(Integer.parseInt(fecha.substring(3, 5)),Integer.parseInt(fecha.substring(6, 10))));
+
+	System.out.println(cambioDeFormato(fecha));
 	entrada.close();
 
 	}
